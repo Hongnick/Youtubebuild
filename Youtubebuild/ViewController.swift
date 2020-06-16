02b55b3fc7a078093693f74d7ccc5acd6280cbaf
Hongnick <nick55002@gmail.com>
2020-06-16 18:53:06 +0800

    inital xcode project creation

---
+++ Youtubebuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtubebuild
+//
+//  Created by 洪敏菖 on 2020/6/16.
+//  Copyright © 2020 Nickhung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
